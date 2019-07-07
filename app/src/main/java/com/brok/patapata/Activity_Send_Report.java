fb906252e22f854e093aab1105e0427d282a0e7f
Brokoth <brianokoth11@gmail.com>
2019-07-08 00:05:23 +0300

    report works

--- app/src/main/java/com/brok/patapata/Activity_Send_Report.java
+++ app/src/main/java/com/brok/patapata/Activity_Send_Report.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,6 +24,7 @@ public class Activity_Send_Report extends AppCompatActivity {
     private String gotid;
     private String mwisho;
     private String input;
+    private String currentuser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,29 +36,29 @@ public class Activity_Send_Report extends AppCompatActivity {
         gotid = getIncomingIntent();
 
 
-        String currentuser = FirebaseAuth.getInstance().getCurrentUser().getUid();
+       currentuser = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
         reportvalue= (EditText) findViewById(R.id.your_report);
-     input = reportvalue.getText().toString();
-if(input==null){
-    Toast.makeText(getApplicationContext(), "Input is null!", Toast.LENGTH_SHORT).show();
-}
+
+
         test = (TextView) findViewById(R.id.test);
 
-        mwisho = "DriverID: " +  gotid  + " UserID: " + currentuser + " " + input;
+
 
 
         reportB= (Button) findViewById(R.id.reportButton);
         reportB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                input = reportvalue.getText().toString();
+                mwisho = "DriverID: " +  gotid  + " UserID: " + currentuser + " " + input;
                 test.setText(mwisho);
 
                 //String report = reportvalue.getText().toString();
-                //FirebaseDatabase.getInstance().getReference("users").child(FirebaseAuth.
-                      //  getInstance().getCurrentUser().getUid()).child("reports").push().setValue(mwisho);
-              //  Intent intent = new Intent(Activity_Send_Report.this, driver.class);
-               // startActivity(intent);
+                FirebaseDatabase.getInstance().getReference("users").child(FirebaseAuth.
+                       getInstance().getCurrentUser().getUid()).child("reports").push().setValue(mwisho);
+                Intent intent = new Intent(Activity_Send_Report.this, driver.class);
+                startActivity(intent);
             }
         });
 
