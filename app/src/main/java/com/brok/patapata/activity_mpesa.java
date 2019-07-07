ff766b0cdb0d04596d74b628efc42893fe7d97ab
Steven-Nyaga <steven.nyaga@strathmore.edu>
2019-07-07 18:53:41 +0300

    new mpesa

--- app/src/main/java/com/brok/patapata/activity_mpesa.java
+++ app/src/main/java/com/brok/patapata/activity_mpesa.java
@@ -20,66 +20,78 @@ import com.androidstudy.daraja.model.LNMResult;
 import com.androidstudy.daraja.util.TransactionType;
 
 public class activity_mpesa extends AppCompatActivity {
-    private EditText meditText;
-    private Button mbutton;
+    EditText editTextPhone;
+    Button btnLipa;
+
+    //declare daraja as a global variable
     Daraja daraja;
-    String phoneNumber;
+    String phonenumber;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mpesa);
 
-        meditText = (EditText) findViewById(R.id.mtext);
-        mbutton = (Button) findViewById(R.id.mpay);
-
-        daraja = Daraja.with("cSuIFLeHAnsOFTAVVlQ3Spd5n6J3sEGr", "Q5XTbAsBLAv29SQv", new DarajaListener<AccessToken>() {
+        editTextPhone =findViewById(R.id.mtext);
+        btnLipa =findViewById(R.id.mpay);
+        btnLipa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //get phone number
+                phonenumber = editTextPhone.getText().toString().trim();
+                if (TextUtils.isEmpty(phonenumber)) {
+                    Toast.makeText(activity_mpesa.this, "please insert phone number", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                LipaNaMpesa(phonenumber);
+            }
+        });
+        //Init daraja
+        daraja = Daraja.with("ioXxKqeAUkSyAMKnQQrTchGDM0RAtG5b", "QGtz94gODybXQm8q", new DarajaListener<AccessToken>() {
             @Override
             public void onResult(@NonNull AccessToken accessToken) {
-                Log.i(activity_mpesa.this.getClass().getSimpleName(), accessToken.getAccess_token());
-                Toast.makeText(activity_mpesa.this, "TOKEN : " + accessToken.getAccess_token(), Toast.LENGTH_SHORT).show();
+                Log.i(activity_mpesa.this.getClass().getSimpleName(),accessToken.getAccess_token());
+                //  Toast.makeText(Payment.this,"TOKEN :"+ accessToken.getAccess_token(),Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onError(String error) {
-                Log.e(activity_mpesa.this.getClass().getSimpleName(), error);
+                Log.e(activity_mpesa.this.getClass().getSimpleName(),error);
+
             }
         });
+    }
 
-        mbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                phoneNumber = meditText.getText().toString().trim();
-                if (TextUtils.isEmpty(phoneNumber)) {
-                    meditText.setError("Please Provide a Phone Number");
-                    return;
-                }
-                LNMExpress lnmExpress = new LNMExpress(
-                        "600347",
-                        "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919",  //https://developer.safaricom.co.ke/test_credentials
-                        TransactionType.CustomerBuyGoodsOnline,
-                        "100",
-                        "254708374149",
-                        "600000",
-                        phoneNumber,
-                        "http://mycallbackurl.com/checkout.php",
-                        "001ABC",
-                        "Goods Payment"
-                );
-                daraja.requestMPESAExpress(lnmExpress,
-                        new DarajaListener<LNMResult>() {
-                            @Override
-                            public void onResult(@NonNull LNMResult lnmResult) {
-                                Log.i(activity_mpesa.this.getClass().getSimpleName(), lnmResult.ResponseDescription);
-                            }
+    private void LipaNaMpesa(String phonenumber) {
+//party A is the number sending the money.It has to be a valid safaricom phone number
+// phonenumber is the mobile number to receive the stk pin prompt.The number can be the sam as partyA.
+//BusinessShort code = PartyB
+        final LNMExpress lnmExpress = new LNMExpress(
+                "174379",
+                "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919",
+                TransactionType.CustomerBuyGoodsOnline,
+                "50",
+                "254708374149",
+                "174379",
+                phonenumber,
+                "http://mycallbackurl.com/checkout.php",
+                "001ABC",
+                "Goods Paymnet"
+        );
+        //actual method
+        daraja.requestMPESAExpress(lnmExpress,
+                new DarajaListener<LNMResult>() {
+                    @Override
+                    public void onResult(@NonNull LNMResult lnmResult) {
+                        Log.i(activity_mpesa.this.getClass().getSimpleName(), lnmResult.ResponseDescription);
+                    }
 
-                            @Override
-                            public void onError(String error) {
-                                Log.i(activity_mpesa.this.getClass().getSimpleName(), error);
-                            }
-                        }
-                );
-            }
-        });
+                    @Override
+                    public void onError(String error) {
+                        Log.i(activity_mpesa.this.getClass().getSimpleName(), error);
+                    }
+                });
+
     }
 }
