53514179f8b06141d54fe37075fdacb435167196
Brokoth <brianokoth11@gmail.com>
2019-07-08 01:15:52 +0300

    Update driverMaps.java

--- app/src/main/java/com/brok/patapata/driverMaps.java
+++ app/src/main/java/com/brok/patapata/driverMaps.java
@@ -99,6 +99,28 @@ public class driverMaps extends FragmentActivity implements OnMapReadyCallback, 
             }
         });
 
+
+
+
+
+
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        userid = getIncomingIntent();
+        mMap = googleMap;
+
         driverid = FirebaseAuth.getInstance().getCurrentUser().getUid();
         //Delete all other requests the driver currently has.
         FirebaseDatabase.getInstance().getReference().child("requests").addListenerForSingleValueEvent(new ValueEventListener() {
@@ -150,6 +172,10 @@ public class driverMaps extends FragmentActivity implements OnMapReadyCallback, 
                         String url = getUrl(driverlocation.getPosition(), userlocation.getPosition(), "driving");
                         new FetchURL(driverMaps.this).execute(url, "driving");
 
+
+                        mMap.addMarker(driverlocation);
+                        mMap.addMarker(userlocation);
+
                     }
 
                     @Override
@@ -168,27 +194,6 @@ public class driverMaps extends FragmentActivity implements OnMapReadyCallback, 
 
 
 
-
-
-    }
-
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        userid = getIncomingIntent();
-
-        mMap = googleMap;
-mMap.addMarker(driverlocation);
-        mMap.addMarker(userlocation);
     }
 
 
